61036d30607a48ceec5f04ae956b00f98b7c6131
shawbrnz <101843749+shawbrnz@users.noreply.github.com>
2023-05-01 10:34:04 +1200

    v4- Triangles
    Made theoretically working hitboxes, need to make GUI to test

--- main.java
+++ main.java
@@ -3,10 +3,11 @@
  * Waterways project
  *
  * @Brendan Shaw
- * @v3 - 28/4
+ * @v4 - 1/5
  * 
  * Need to:
  * 1- GUI,
+ * CLEAN UP!
  * 2- Clean changing system,
  * 3- Water,
  * 4- File stuff
@@ -19,12 +20,21 @@ import javax.swing.*;
 import javax.swing.JFrame;   
 import java.awt.Dimension;
 import java.awt.event.*;
-public class main extends JFrame
+public class main extends JFrame implements ActionListener, MouseListener
 {
+    //Sets up text scanner               TEMP
     Scanner scanner = new Scanner(System.in);
+    //Offset the grid is from the UI
+    int xOffset=0;
+    int yOffset=10;
+    //Number of squares
     int xSize=10;
     int ySize=10;
+    //Size of squares
+    int squareSize=10;
+    //Array of the pipes. Third dimension is the edges
     boolean [][][] pipesArray=new boolean[xSize][ySize][4];
+    //Keeps program running             TEMP
     boolean keepRunning=true;
     public main()
     {
@@ -36,7 +46,7 @@ public class main extends JFrame
                     Integer.valueOf(data[1]),
                     Integer.valueOf(data[2])};
             pipesArray[pipeLocation[0]][pipeLocation[1]][pipeLocation[2]]=
-            swap(pipesArray[pipeLocation[0]][pipeLocation[1]][pipeLocation[2]]);
+            !(pipesArray[pipeLocation[0]][pipeLocation[1]][pipeLocation[2]]);
             JPanel panel = new JPanel();
             panel.setPreferredSize(new Dimension(400,400));
             Canvas myGraphic=new Canvas();
@@ -59,10 +69,91 @@ public class main extends JFrame
             }
         }
     }
-    boolean swap(boolean swapper){
-        if(swapper){
-            return false;
+
+    public void actionPerformed(ActionEvent e){
+        String itemClicked=e.getActionCommand();
+
+    }
+
+    public void mouseExited(MouseEvent e){}
+
+    public void mouseEntered(MouseEvent e){}
+
+    public void mouseReleased(MouseEvent e){}
+
+    public void mousePressed(MouseEvent e){}
+
+    public void mouseClicked(MouseEvent e){
+        System.out.println(e);
+        int xMouse=e.getX()-xOffset;
+        int yMouse=e.getY()-yOffset;
+        if(xMouse>0&&yMouse>0){
+            //Need to clean
+            pipesArray[xMouse/squareSize][yMouse/squareSize]
+            [subSquares(yMouse%squareSize,xMouse%squareSize,squareSize)]=
+            !(pipesArray[xMouse/squareSize][yMouse/squareSize]
+            [subSquares(yMouse%squareSize,xMouse%squareSize,squareSize)]);
         }
-        return true;
+    }
+
+    /*    !
+    boolean swap(boolean swapper){
+    if(swapper){
+    return false;
+    }
+    return true;
+    }*/
+    
+    //Returns the side of the square that is clicked, 0 being top, going clockwise, left being 3
+    int subSquares(int yClicked, int xClicked, int squareSize){
+        //It is possible to make this slightly faster by splitting the left
+        //and right up and only checking 1 and 3 once
+
+        //TODO- Clean this
+
+        //Top right
+        if(squareSize/2>=xClicked&&squareSize/2>=yClicked){
+            if(triangleHitBoxMethod(xClicked-(squareSize/2),-(yClicked))){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+        //Top left
+        if(squareSize/2<xClicked&&squareSize/2>yClicked){
+            if(triangleHitBoxMethod(xClicked,yClicked)){
+                return 0;
+            }else{
+                return 3;
+            }
+        }
+        //Bottom right
+        if(squareSize/2>xClicked&&squareSize/2<yClicked){
+            if(triangleHitBoxMethod(xClicked-(squareSize/2),yClicked-(squareSize/2))){
+                return 1;
+            }else{
+                return 2;
+            }
+        }
+        //Bottom left
+        if(squareSize/2<=xClicked&&squareSize/2<=yClicked){
+            if(triangleHitBoxMethod(xClicked,-(yClicked-(squareSize/2)))){
+                return 2;
+            }else{
+                return 3;
+            }
+        }
+
+        //Should not run, but is required for java. Will be removed after cleaning
+        return -1;
+    }
+    //Buttons are squares not triangles, so I have to make my own
+    boolean triangleHitBoxMethod(int yClicked, int xClicked){
+        //Method to find if top or bottom was clicked
+        //I don't care if it is clicked on the line because
+        //user likely won't notice unless they go out of their 
+        //way, does not break, just returns false, which is 1
+        //pixel off of the area that is meant to return false
+        return xClicked>yClicked;
     }
 }
